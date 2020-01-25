24baceb88636a757d609b4c5800b5dc2ec41beda
Dennis Padiernos <depadiernos@gmail.com>
2020-01-24 19:37:44 -0600

    create authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,24 @@
+const { jwt } = require("jsonwebtoken")
+const jwtSecret = process.env.JWT_SECRET || "this is my secret passphrase"
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+  const notAllowed = { you: "shall not pass!" }
+  const token = req.headers.authorization
+
+  if (!token) {
+    res.status(401).json(notAllowed)
+  }
+  jwt.verify(token, jwtSecret, (err, { sub, username }) => {
+    if (err) {
+      res.status(401).json(notAllowed)
+    } else {
+      req.user = { id: sub, username }
+    }
+  })
+  next()
+}
